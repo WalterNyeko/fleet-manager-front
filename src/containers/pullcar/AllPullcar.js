b0009c57cd8e21d39b579f5055e9ec9a4a50bb36
Oyee James <engjames256@gmail.com>
2019-11-09 02:25:31 +0300

    Added table for viewing all pullcar planner

---
+++ src/containers/pullcar/AllPullcar.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import AllPullcarComponent from "../../components/pullcar/AllPullcar";
+
+export default class AllPullcar extends Component {
+  render() {
+    return (
+      <Fragment>
+        <AllPullcarComponent />
+      </Fragment>
+    );
+  }
+}
