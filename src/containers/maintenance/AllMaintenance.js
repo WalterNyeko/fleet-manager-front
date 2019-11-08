97f82911e95009f6485e1ad37c23b98aa685c06f
Oyee James <engjames256@gmail.com>
2019-11-09 02:18:00 +0300

    Added table for viewing all maintenance

---
+++ src/containers/maintenance/AllMaintenance.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import AllMaintenanceComponent from "../../components/maintenanace/AllMaintenance";
+
+export default class AllMaintenance extends Component {
+  render() {
+    return (
+      <Fragment>
+        <AllMaintenanceComponent />
+      </Fragment>
+    );
+  }
+}
