a9f105b472cb709bf78b5c8c427c19ecc2ef643d
Oyee James <engjames256@gmail.com>
2019-11-12 10:20:49 +0300

    Added form for creating new vehicle offroad

---
+++ src/containers/vehicles/VehicleOffRoad.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment  } from  "react";
+import VehicleOffRoadComponent from "../../components/vehicles/VehicleOffRoad";
+
+class  VehicleOffRoad extends Component {
+    render() { 
+        return (  
+            <Fragment>
+            <VehicleOffRoadComponent/>
+            </Fragment>
+        );
+    }
+}
+ 
+export default VehicleOffRoad;
