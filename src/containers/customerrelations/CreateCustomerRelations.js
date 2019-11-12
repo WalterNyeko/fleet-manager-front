a834f999bd2d551a98734777a49d14d8390ac789
Oyee James <engjames256@gmail.com>
2019-11-12 14:47:19 +0300

    Added form for creating customer relations

---
+++ src/containers/customerrelations/CreateCustomerRelations.js
@@ -0,0 +1,12 @@
+import React, { Component, Fragment } from "react";
+import CreateCustomerRelationsComponent from "../../components/customerrelations/CreateCustomer Relations";
+
+export default class CreateCustomerRelations extends Component {
+  render() {
+    return (
+      <Fragment>
+        <CreateCustomerRelationsComponent />
+      </Fragment>
+    );
+  }
+}
