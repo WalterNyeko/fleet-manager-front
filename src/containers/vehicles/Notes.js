69e0e43ccb87be2f6b4b1e6be0731491a1ddad3a
Oyee James <engjames256@gmail.com>
2019-11-13 12:39:31 +0300

    Added for for creating notes on the vehicle

---
+++ src/containers/vehicles/Notes.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import NotesComponent from "../../components/vehicles/VehicleAllocation";
+
+class Notes extends Component {
+  render() {
+    return (
+      <Fragment>
+        <NotesComponent />
+      </Fragment>
+    );
+  }
+}
+
+export default Notes;
