79cc03a811dc47028edd44cf87832713d19d18ab
Oyee James <engjames256@gmail.com>
2019-11-13 11:03:47 +0300

    Added form for creating vehicle allocatiomn

---
+++ src/containers/vehicles/VehicleAllocation.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import VehicleAllocationComponent from "../../components/vehicles/VehicleAllocation";
+
+class VehicleAllocation extends Component {
+  render() {
+    return (
+      <Fragment>
+        <VehicleAllocationComponent />
+      </Fragment>
+    );
+  }
+}
+
+export default VehicleAllocation;
