0d81a352ca37f256c72b1f58dcd0cc5fb46934dd
Oyee James <engjames256@gmail.com>
2019-11-14 10:21:15 +0300

    Added form foir creating repair description

---
+++ src/containers/vehicles/RepairDescription.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import RepairDescriptionComponent from "../../components/vehicles/RepairDescription";
+
+class RepairDescription extends Component {
+  render() {
+    return (
+      <Fragment>
+        <RepairDescriptionComponent />
+      </Fragment>
+    );
+  }
+}
+
+export default RepairDescription;
