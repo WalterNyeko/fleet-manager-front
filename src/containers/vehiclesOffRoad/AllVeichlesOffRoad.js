fba73bbe91b63480ccab846ee46835d3a2dcbc59
Oyee James <engjames256@gmail.com>
2019-11-14 18:13:54 +0300

    Added form for creating offroad information

--- src/containers/vehiclesOffRoad/AllVeichlesOffRoad.js
+++ src/containers/vehiclesOffRoad/AllVeichlesOffRoad.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from "react";
-import AllVeichlesOffRoadComponent from "../../components/vehicles/AllVeichlesOffRoad";
+import AllVeichlesOffRoadComponent from "../../components/VehiclesOffRoad/AllVeichlesOffRoad";
 
 export default class AllVeichlesOffRoad extends Component {
   render() {
