343644bc201509273a83953b554a575daa56487e
Oyee James <engjames256@gmail.com>
2019-11-14 15:04:39 +0300

    Added form for creating vehicle hire information

---
+++ src/containers/veichlehire/HireInfo.js
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+import HireInfoComponent from "../../components/vehicles/RepairInfo";
+
+class HireInfo extends Component {
+  render() {
+    return (
+      <Fragment>
+        <HireInfoComponent />
+      </Fragment>
+    );
+  }
+}
+
+export default HireInfo;
