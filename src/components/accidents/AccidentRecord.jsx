d2353174bf566c4804ff6a6e3b7a89a8e0164477
Oyee James <engjames256@gmail.com>
2019-11-17 01:36:28 +0300

    Updated all vehicle tables to be scrollable

--- src/components/accidents/AccidentRecord.jsx
+++ src/components/accidents/AccidentRecord.jsx
@@ -4,79 +4,63 @@ export default function AccidentRecord() {
   return (
     <div class="row">
       <div class="card">
-        <div class="card-body">
-          <h4 class="mt-0 header-title">Veichle Accidents</h4>
-          <table
-            id="datatable-buttons"
-            class="table table-striped table-bordered dt-responsive nowrap"
-            style={{
-              borderCollapse: "collapse",
-              borderSpacing: 0,
-              width: "100%"
-            }}
-          >
-            <thead>
-              <tr>
-                <th>Accident Ref.</th>
-                <th>Registration No.</th>
-                <th>Accident Status</th>
-                <th>Registration No.</th>
-                <th>Extra Registration No.</th>
-                <th>Third Party Involved</th>
-                <th>Total Cost (Net)</th>
-                <th>Accident Date</th>
-                <th>Driver Name</th>
-                <th>Make Code</th>
-                <th>Model Code</th>
-                <th>Division/Client</th>
-                <th>Comment</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-                <td>Accident Date</td>
-                <td>Driver Name</td>
-                <td>Make Code</td>
-                <td>Model Code</td>
-                <td>Division/Client</td>
-                <td>Comment</td>
-              </tr>
-              <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
-                <td>Accident Date</td>
-                <td>Driver Name</td>
-                <td>Make Code</td>
-                <td>Model Code</td>
-                <td>Division/Client</td>
-                <td>Comment</td>
-              </tr>
-              <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>San Francisco</td>
-                <td>66</td>
-                <td>2009/01/12</td>
-                <td>$86,000</td>
-                <td>Accident Date</td>
-                <td>Driver Name</td>
-                <td>Make Code</td>
-                <td>Model Code</td>
-                <td>Division/Client</td>
-                <td>Comment</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="card col-md-12 my-card ml-1">
+          <div className="card-body custom-card">
+            <div className="row">
+              <div class="card-body" style={{ "overflow-x": "auto", "overflow-y": "auto" }}>
+                <table
+                  id="datatable-buttons"
+                  class="table table-striped table-bordered dt-responsive nowrap"
+                  style={{
+                    borderCollapse: "collapse",
+                    borderSpacing: 0,
+                    width: "100%"
+                  }}
+                >
+                  <thead>
+                    <tr>
+                      <th>Accident Ref.</th>
+                      <th>Cost Type</th>
+                      <th>Supplier</th>
+                      <th>Entry Date</th>
+                      <th>Estimate Number</th>
+                      <th>Estimate Value</th>
+                      <th>Actual Value</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Accident Ref.</td>
+                      <td>Cost Type</td>
+                      <td>Supplier</td>
+                      <td>Entry Date</td>
+                      <td>Estimate Number</td>
+                      <td>Estimate Value</td>
+                      <td>Actual Value</td>
+                    </tr>
+                    <tr>
+                    <td>Accident Ref.</td>
+                      <td>Cost Type</td>
+                      <td>Supplier</td>
+                      <td>Entry Date</td>
+                      <td>Estimate Number</td>
+                      <td>Estimate Value</td>
+                      <td>Actual Value</td>
+                    </tr>
+                    <tr>
+                    <td>Accident Ref.</td>
+                      <td>Cost Type</td>
+                      <td>Supplier</td>
+                      <td>Entry Date</td>
+                      <td>Estimate Number</td>
+                      <td>Estimate Value</td>
+                      <td>Actual Value</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/accidents/VehicleRepairs.jsx
+++ src/components/accidents/VehicleRepairs.jsx
@@ -4,87 +4,119 @@ export default function VehicleRepairs() {
   return (
     <div class="row">
       <div class="card">
-        <div class="card-body">
-          <h4 class="mt-0 header-title">Veichle Repairs</h4>
-          <table
-            id="datatable-buttons"
-            class="table table-striped table-bordered dt-responsive nowrap"
-            style={{
-              borderCollapse: "collapse",
-              borderSpacing: 0,
-              width: "100%"
-            }}
-          >
-            <thead>
-              <tr>
-                <th>Job Authorization No.</th>
-                <th>Workshop</th>
-                <th>Job Status</th>
-                <th>Registration No.</th>
-                <th>Extra Registration No.</th>
-                <th>Model Code</th>
-                <th>Division/Client</th>
-                <th>Cost Center</th>
-                <th>Odometer In</th>
-                <th>Reason</th>
-                <th>Job Date</th>
-                <th>Job End Date</th>
-                <th>Invoice</th>
-                <th>Total Invoice</th>
-                <th>Comments</th>
-                <th>Paperwork Status</th>
-                <th>Cost Labour</th>
-                <th>Created By</th>
-                <th>Date</th>
-                <th>Changed By</th>
-                <th>Date</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>Tiger Nixon</td>
-                <td>System Architect</td>
-                <td>Edinburgh</td>
-                <td>61</td>
-                <td>2011/04/25</td>
-                <td>$320,800</td>
-                <td>Accident Date</td>
-                <td>Driver Name</td>
-                <td>Make Code</td>
-                <td>Model Code</td>
-                <td>Division/Client</td>
-                <td>Comment</td>
-              </tr>
-              <tr>
-                <td>Garrett Winters</td>
-                <td>Accountant</td>
-                <td>Tokyo</td>
-                <td>63</td>
-                <td>2011/07/25</td>
-                <td>$170,750</td>
-                <td>Accident Date</td>
-                <td>Driver Name</td>
-                <td>Make Code</td>
-                <td>Model Code</td>
-                <td>Division/Client</td>
-                <td>Comment</td>
-              </tr>
-              <tr>
-                <td>Ashton Cox</td>
-                <td>Junior Technical Author</td>
-                <td>San Francisco</td>
-                <td>66</td>
-                <td>2009/01/12</td>
-                <td>$86,000</td>
-                <td>Accident Date</td>
-                <td>Driver Name</td>
-                <td>Make Code</td>
-                <td>Model Code</td>
-                <td>Division/Client</td>
-                <td>Comment</td>
-              </tr>
-            </tbody>
-          </table>
+        <div className="card col-md-12 my-card ml-1">
+          <div className="card-body custom-card">
+            <div className="row">
+              <div class="card-body" style={{ "overflow-x": "auto", "overflow-y": "auto" }}>
+                <table
+                  id="datatable-buttons"
+                  class="table table-striped table-bordered dt-responsive nowrap"
+                  style={{
+                    borderCollapse: "collapse",
+                    borderSpacing: 0,
+                    width: "100%"
+                  }}
+                >
+                  <thead>
+                    <tr>
+                      <th>Job Authorization No.</th>
+                      <th>Workshop</th>
+                      <th>Job Status</th>
+                      <th>Registration No.</th>
+                      <th>Extra Registration No.</th>
+                      <th>Model Code</th>
+                      <th>Division/Client</th>
+                      <th>Cost Center</th>
+                      <th>Odometer In</th>
+                      <th>Reason</th>
+                      <th>Job Date</th>
+                      <th>Job End Date</th>
+                      <th>Invoice</th>
+                      <th>Total Invoice</th>
+                      <th>Comments</th>
+                      <th>Paperwork Status</th>
+                      <th>Cost Labour</th>
+                      <th>Created By</th>
+                      <th>Date</th>
+                      <th>Changed By</th>
+                      <th>Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>Tiger Nixon</td>
+                      <td>System Architect</td>
+                      <td>Edinburgh</td>
+                      <td>61</td>
+                      <td>2011/04/25</td>
+                      <td>$320,800</td>
+                      <td>Accident Date</td>
+                      <td>Driver Name</td>
+                      <td>Make Code</td>
+                      <td>Model Code</td>
+                      <td>Division/Client</td>
+                      <td>Comment</td>
+                      <td>Invoice</td>
+                      <td>Total Invoice</td>
+                      <td>Comments</td>
+                      <td>Paperwork Status</td>
+                      <td>Cost Labour</td>
+                      <td>Created By</td>
+                      <td>Date</td>
+                      <td>Changed By</td>
+                      <td>Date</td>
+                    </tr>
+                    <tr>
+                      <td>Garrett Winters</td>
+                      <td>Accountant</td>
+                      <td>Tokyo</td>
+                      <td>63</td>
+                      <td>2011/07/25</td>
+                      <td>$170,750</td>
+                      <td>Accident Date</td>
+                      <td>Driver Name</td>
+                      <td>Make Code</td>
+                      <td>Model Code</td>
+                      <td>Division/Client</td>
+                      <td>Comment</td>
+                      <td>Invoice</td>
+                      <td>Total Invoice</td>
+                      <td>Comments</td>
+                      <td>Paperwork Status</td>
+                      <td>Cost Labour</td>
+                      <td>Created By</td>
+                      <td>Date</td>
+                      <td>Changed By</td>
+                      <td>Date</td>
+                    </tr>
+                    <tr>
+                      <td>Ashton Cox</td>
+                      <td>Junior Technical Author</td>
+                      <td>San Francisco</td>
+                      <td>66</td>
+                      <td>2009/01/12</td>
+                      <td>$86,000</td>
+                      <td>Accident Date</td>
+                      <td>Driver Name</td>
+                      <td>Make Code</td>
+                      <td>Model Code</td>
+                      <td>Division/Client</td>
+                      <td>Comment</td>
+                      <td>Invoice</td>
+                      <td>Total Invoice</td>
+                      <td>Comments</td>
+                      <td>Paperwork Status</td>
+                      <td>Cost Labour</td>
+                      <td>Created By</td>
+                      <td>Date</td>
+                      <td>Changed By</td>
+                      <td>Date</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
