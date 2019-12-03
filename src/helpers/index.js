94da89361e4d2310869fbbe66eef95af9c2012bb
WalterNyeko <nyekowalter69@gmail.com>
2019-12-04 00:17:45 +0300

    introduce redux

---
+++ src/helpers/index.js
@@ -0,0 +1,41 @@
+import { notification } from "antd";
+
+/**
+ * ensures that we can render a success message to the user
+ * after their requests for particular resources were successful
+ *
+ * @param {String} message
+ *
+ * @returns {void}
+ */
+export const showSuccessNotification = message => {
+  const data = {
+    message: message,
+    description: "Successful request",
+    duration: 5
+  };
+  notification.success(data);
+};
+
+/**
+ * ensures that we can render an error message to the user
+ * after their requests for particular resources were not successful
+ *
+ * @param {String} message
+ *
+ * @returns {void}
+ */
+export const showErrorNotification = message => {
+  const data = {
+    message: message,
+    description: "Error occured, please check the error and try again",
+    duration: 8
+  };
+  notification.error(data);
+};
+
+export const validateEmail = email => {
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const result = re.test(String(email).toLowerCase());
+  return result;
+};
