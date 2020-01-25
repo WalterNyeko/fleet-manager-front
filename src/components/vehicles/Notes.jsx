1dcd4ab506e807d8921b8a4e4bdd177d6c93ef32
Oyee James <engjames256@gmail.com>
2020-01-25 19:10:48 +0300

    Implemented Vehicle Major Components Information

--- src/components/vehicles/Notes.jsx
+++ src/components/vehicles/Notes.jsx
@@ -4,48 +4,48 @@ import "react-datepicker/dist/react-datepicker.css";
 
 export default function Notes({ handleDateChange, date }) {
   return (
-    <div className="row">
-      <form className="row" action="#">
-        <div className="card col-md-12 my-card ml-4">
-          <div className="card-body custom-card">
-            <div className="row">
-              <div className="form-group col-md-3">
+    <div className='row'>
+      <form className='row' action='#'>
+        <div className='card col-md-12 my-card ml-4'>
+          <div className='card-body custom-card'>
+            <div className='row'>
+              <div className='form-group col-md-3'>
                 <label>Date</label> <br />
                 <DatePicker
                   selected={date}
                   onChange={handleDateChange}
-                  className="form-control"
+                  className='form-control'
                 />
               </div>
-              <div className="form-group col-md-3">
+              <div className='form-group col-md-3'>
                 <label>Contract Baloon Payment</label>{" "}
                 <input
-                  type="text"
-                  className="form-control"
+                  type='text'
+                  className='form-control'
                   required
-                  placeholder=""
+                  placeholder=''
                 />
               </div>
-              <div className="form-group col-md-3">
+              <div className='form-group col-md-3'>
                 <label>Notes</label>{" "}
                 <input
-                  type="text"
-                  className="form-control"
+                  type='text'
+                  className='form-control'
                   required
-                  placeholder=""
+                  placeholder=''
                 />
               </div>
-              <div className="form-group col-md-3 mb-0">
+              <div className='form-group col-md-3 mb-0'>
                 <div>
                   <button
-                    type="submit"
-                    className="btn btn-primary waves-effect waves-light mr-1"
+                    type='submit'
+                    className='btn btn-primary waves-effect waves-light mr-1'
                   >
                     Submit
                   </button>{" "}
                   <button
-                    type="reset"
-                    className="btn btn-secondary waves-effect"
+                    type='reset'
+                    className='btn btn-secondary waves-effect'
                   >
                     Cancel
                   </button>
